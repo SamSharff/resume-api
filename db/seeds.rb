2c9c8d5d829c3960ba4dd71471fa4078c828f3ec
Sam Sharff <samanthaleec@gmail.com>
2022-04-21 17:04:06 -0400

    installs seed dump gem; dumps seeded data; drops db; reseeds and migrates database; adds creates user successfully in requests.http file; fixes secure password issue

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,37 +1,19 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
-
-# user = User.create({name: "Evelyn", email: "Evelyn@example.com"})
-# user = User.create({name: "Sam", email: "Sam@example.com"})
-# user = User.create({name: "Aaron", email: "Aaron@example.com"})
-# experience = Experience.create({user_id: 1, category: "Job", org_or_institution: "UNFI", description: "Managed the buying portfolio of and day-to-day communications for two buyers in the purchasing department at a major natural and organic food distribution company.", title: "Purchasing Associate", dates: "2011-2012", location: "NH", misc: "data analysis and visualization responsibilities"})
-# experience.save
-# experience = Experience.create({user_id: 1, category: "Job", org_or_institution: "Norwich Public Library", description: "Managed all aspects of the library’s public relations efforts, adult programming, and contracts for use of space. Tracked and reported programming statistics; worked closely with volunteers, patrons, and Friends group.", title: "Programming & Publicity Cooridnator", dates: "2014-2015", location: "VT", misc: "libraries"})
-# experience.save
-
-# experience = Experience.create({user_id: 1, category: "Skill", description: "Ruby"})
-# experience.save
-
-# experience = Experience.create({user_id: 1, category: "Skill", description: "Rails"})
-# experience.save
-
-# experience = Experience.create({user_id: 1, category: "Volunteer work", org_or_institution: "Brattleboro Community Justice Center", description: "Trained team member. Supported sex offender’s transition from prison back to community.", title: "Team Member", dates: "2011-2012", location: "VT"})
-# experience.save
-
-# experience = Experience.create({user_id: 2, category: "Volunteer work", org_or_institution: "Marriage", description: "Married Sam. It was awesome.", title: "Husband!", dates: "2012-present", location: "VT"})
-# experience.save
-
-# training = Training.create({user_id: 2, category: "Education", org_or_institution: "University of New Hamsphire", description: "BA in Theatre, some physics", dates: "2005-2007", degree_or_cert: "BA in theatre", location: "NH", misc: "College"})
-# experience.save
-
-# experience = Experience.create({user_id: 3, category: "Skills", description: "Commander, Sharff fam"})
-# experience.save
-
-# training = Training.create({user_id: 3, category: "Professional dev", org_or_institution: "CRES Elementary School", description: "Kindergarten, 100+ days", dates: "2021-2022", degree_or_cert: "N/A", location: "ME", misc: "Showing kindergarten who's boss, adorably."})
-# experience.save
+# User.create!([
+#   {name: "Sam", email: "Sam@example.com", password: "password"},
+#   {name: "Aaron", email: "Aaron@example.com", password: "password"},
+#   {name: "Evelyn", email: "Evelyn@example.com", password: "password"},
+#   {name: "Test name", email: "test@test.com", password: "password"}])
+# Training.create!([
+#   {user_id: 2, category: "Education", org_or_institution: "University of New Hamsphire", description: "BA in Theatre, some physics", dates: "2005-2007", degree_or_cert: "BA in theatre", location: "NH", misc: "College"},
+#   {user_id: 3, category: "Professional dev", org_or_institution: "CRES Elementary School", description: "Kindergarten, 100+ days", dates: "2021-2022", degree_or_cert: "N/A", location: "ME", misc: "Showing kindergarten who's boss, adorably."}
+# ])
+# Experience.create!([
+#   {user_id: 1, category: "Job", org_or_institution: "UNFI", description: "Managed the buying portfolio of and day-to-day communications for two buyers in the purchasing department at a major natural and organic food distribution company.", title: "Purchasing Associate", dates: "2011-2012", location: "NH", misc: "data analysis and visualization responsibilities"},
+#   {user_id: 1, category: "Job", org_or_institution: "Norwich Public Library", description: "Managed all aspects of the library’s public relations efforts, adult programming, and contracts for use of space. Tracked and reported programming statistics; worked closely with volunteers, patrons, and Friends group.", title: "Programming & Publicity Cooridnator", dates: "2014-2015", location: "VT", misc: "libraries"},
+#   {user_id: 1, category: "Skill", org_or_institution: nil, description: "Ruby", title: nil, dates: nil, location: nil, misc: nil},
+#   {user_id: 1, category: "Skill", org_or_institution: nil, description: "Rails", title: nil, dates: nil, location: nil, misc: nil},
+#   {user_id: 1, category: "Volunteer work", org_or_institution: "Brattleboro Community Justice Center", description: "Trained team member. Supported sex offender’s transition from prison back to community.", title: "Team Member", dates: "2011-2012", location: "VT", misc: nil},
+#   {user_id: 2, category: "Volunteer work", org_or_institution: "Marriage", description: "Married Sam. It was awesome.", title: "Husband!", dates: "2012-present", location: "VT", misc: nil},
+#   {user_id: 3, category: "Skills", org_or_institution: nil, description: "Commander, Sharff fam", title: nil, dates: nil, location: nil, misc: nil}
+# ])
 
