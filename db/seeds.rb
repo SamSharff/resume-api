e58f5b5b21021f6a1fb6530e004c17e2e45da466
Sam Sharff <samanthaleec@gmail.com>
2022-04-21 15:27:36 -0400

    adds instances of data

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,5 +9,29 @@
 # user = User.create({name: "Evelyn", email: "Evelyn@example.com"})
 # user = User.create({name: "Sam", email: "Sam@example.com"})
 # user = User.create({name: "Aaron", email: "Aaron@example.com"})
-experience = Experience.create({user_id: 1, type: "Job", org_or_institution: "UNFI", description: "Managed the buying portfolio of and day-to-day communications for two buyers in the purchasing department at a major natural and organic food distribution company.", title: "Purchasing Associate", dates: "2011-2012", location: "NH", misc: "data analysis and visualization responsibilities"})
+# experience = Experience.create({user_id: 1, category: "Job", org_or_institution: "UNFI", description: "Managed the buying portfolio of and day-to-day communications for two buyers in the purchasing department at a major natural and organic food distribution company.", title: "Purchasing Associate", dates: "2011-2012", location: "NH", misc: "data analysis and visualization responsibilities"})
+# experience.save
+# experience = Experience.create({user_id: 1, category: "Job", org_or_institution: "Norwich Public Library", description: "Managed all aspects of the library’s public relations efforts, adult programming, and contracts for use of space. Tracked and reported programming statistics; worked closely with volunteers, patrons, and Friends group.", title: "Programming & Publicity Cooridnator", dates: "2014-2015", location: "VT", misc: "libraries"})
+# experience.save
+
+# experience = Experience.create({user_id: 1, category: "Skill", description: "Ruby"})
+# experience.save
+
+# experience = Experience.create({user_id: 1, category: "Skill", description: "Rails"})
+# experience.save
+
+# experience = Experience.create({user_id: 1, category: "Volunteer work", org_or_institution: "Brattleboro Community Justice Center", description: "Trained team member. Supported sex offender’s transition from prison back to community.", title: "Team Member", dates: "2011-2012", location: "VT"})
+# experience.save
+
+# experience = Experience.create({user_id: 2, category: "Volunteer work", org_or_institution: "Marriage", description: "Married Sam. It was awesome.", title: "Husband!", dates: "2012-present", location: "VT"})
+# experience.save
+
+# training = Training.create({user_id: 2, category: "Education", org_or_institution: "University of New Hamsphire", description: "BA in Theatre, some physics", dates: "2005-2007", degree_or_cert: "BA in theatre", location: "NH", misc: "College"})
+# experience.save
+
+experience = Experience.create({user_id: 3, category: "Skills", description: "Commander, Sharff fam"})
 experience.save
+
+training = Training.create({user_id: 3, category: "Professional dev", org_or_institution: "CRES Elementary School", description: "Kindergarten, 100+ days", dates: "2021-2022", degree_or_cert: "N/A", location: "ME", misc: "Showing kindergarten who's boss, adorably."})
+experience.save
+
