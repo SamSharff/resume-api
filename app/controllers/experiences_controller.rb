96ccc6279211b49b30c60f9b02b40f924e7195df
Sam Sharff <samanthaleec@gmail.com>
2022-04-27 10:11:05 -0400

    adds route to experiences create action; adds create action to experiences controller

--- app/controllers/experiences_controller.rb
+++ app/controllers/experiences_controller.rb
@@ -3,4 +3,21 @@ class ExperiencesController < ApplicationController
     experiences = Experience.all
     render json: experiences.as_json
   end
+
+  def create
+  experience = Experience.new(
+    user_id: params["user_id"],
+    category: params["category"],
+    org_or_institution: params["org_or_institution"],
+    description: params["description"],
+    title: params["title"],
+    dates: params["date"],
+    location: params["location"],
+    misc: params["misc"]
+  )
+
+  experience.save
+  render json: experience.as_json
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   post "/users" => "users#create"
 
   get "/experiences" => "experiences#index"
+  post "/experiences" => "experiences#create"
 end
