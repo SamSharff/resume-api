9725da2f1029259d86ca91c8cfe5eba4a5c8716e
Sam Sharff <samanthaleec@gmail.com>
2022-04-27 11:31:53 -0400

    adds route to experiences update action; adds update action to experiences controller

--- app/controllers/experiences_controller.rb
+++ app/controllers/experiences_controller.rb
@@ -26,4 +26,21 @@ class ExperiencesController < ApplicationController
 
     render json: experience.as_json
   end
+
+  def update
+    experience_id = params["id"]
+    experience = Experience.find_by(id: experience_id)
+
+    experience.user_id = params["user_id"] || experience.user_id
+    experience.category = params["category"] || experience.category
+    experience.org_or_institution = params["org_or_institution"] || experience.org_or_institution
+    experience.description = params["description"] || experience.description
+    experience.title = params["title"] || experience.title
+    experience.dates = params["date"] || experience.dates
+    experience.location = params["location"] || experience.location
+    experience.misc = params["misc"] || experience.misc
+
+    experience.save
+    render json: experience.as_json
+  end 
 end
