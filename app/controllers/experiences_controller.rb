4ddfb24b700c5a8fd25413981a4f1b61f6ed7040
Sam Sharff <samanthaleec@gmail.com>
2022-05-04 09:27:59 -0400

    updates syntactical issues and further tests current_user helper method in experiences controller

--- app/controllers/experiences_controller.rb
+++ app/controllers/experiences_controller.rb
@@ -10,13 +10,14 @@ class ExperiencesController < ApplicationController
   def create
     experience = Experience.new(
     user_id: current_user.id,
-    category: params["category"],
-    org_or_institution: params["org_or_institution"],
-    description: params["description"],
-    title: params["title"],
-    dates: params["dates"],
-    location: params["location"],
-    misc: params["misc"]
+    # which means user_id is whoever is logged in--you can't enter it in your params (requests.http example)
+    category: params[:category],
+    org_or_institution: params[:org_or_institution],
+    description: params[:description],
+    title: params[:title],
+    dates: params[:dates],
+    location: params[:location],
+    misc: params[:misc]
   )
 
     experience.save
@@ -24,25 +25,25 @@ class ExperiencesController < ApplicationController
   end
 
   def show
-    experience_id = current_user.experiences.find_by(id: params["id"])
-    # experience_id = params["id"]
+    experience_id = current_user.experiences.find_by(id: params[:id])
+    # experience_id = params[:id]
     experience = Experience.find_by(id: experience_id)
 
     render json: experience.as_json
   end
 
   def update
-    experience_id = params["id"]
+    experience_id = params[:id]
     experience = Experience.find_by(id: experience_id)
 
-    experience.user_id = params["user_id"] || experience.user_id
-    experience.category = params["category"] || experience.category
-    experience.org_or_institution = params["org_or_institution"] || experience.org_or_institution
-    experience.description = params["description"] || experience.description
-    experience.title = params["title"] || experience.title
-    experience.dates = params["dates"] || experience.dates
-    experience.location = params["location"] || experience.location
-    experience.misc = params["misc"] || experience.misc
+    experience.user_id = params[:user_id] || experience.user_id
+    experience.category = params[:category] || experience.category
+    experience.org_or_institution = params[:org_or_institution] || experience.org_or_institution
+    experience.description = params[:description] || experience.description
+    experience.title = params[:title] || experience.title
+    experience.dates = params[:dates] || experience.dates
+    experience.location = params[:location] || experience.location
+    experience.misc = params[:misc] || experience.misc
 
     experience.save
     render json: experience.as_json
